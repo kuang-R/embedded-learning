13b26ade2b736c06d113d74e48644051edc5cec0
kuangR <a1173522112@163.com>
2019-07-31 09:50:48 +0800

    更新串口1

--- 标准库-MDK/User/main.c
+++ 标准库-MDK/User/main.c
@@ -1,8 +1,14 @@
 #include "stm32f10x.h"   // 相当于51单片机中的  #include <reg51.h>
+#include "usart1.h"
+#include <stdio.h>
 
 int main(void)
 {
 	// 来到这里的时候，系统的时钟已经被配置成72M。
+	USART1_Config(115200);
+	while (1) {
+		
+	}
 }
 
 
